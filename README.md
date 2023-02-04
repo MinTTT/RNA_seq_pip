97f570f3d08e7aa4a9fcac3fbe221fe663ef9166
MinTTT <pan_chu@outlook.com>
2023-02-04 14:51:29 +0800

    amend

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 
 These codes are written for analyzing the next generation data, not only for mRNA seq data.
 
-We have verified the data types for analyzing: \
+We have verified the data types for analyzing: 
 * RNA seq
 * Deep seq
 
 ### RNA seq
-the script `RNA_seq_pip_CP`
+the script `RNA_seq_pip_CP` was written for mRNA-seq analysis
+
 
--- RNA_seq_pip_CP.py
+++ RNA_seq_pip_CP.py
@@ -87,16 +87,16 @@ for sample in sample_fa_files:
         fa2 = os.path.join(sampleinfo['samples'][sample_name][1], sample_name, replicate + '_2' + seq_data_suffix)
         if not os.path.exists(fa2):
            fa2 = None     
-
-        process_pip = RNASeqAnalyzer(sample_name_rep, seq_ps1=fa1, seq_ps2=fa2, bowtie_pars={"-p": 32}, output_dir=sampleinfo['save_dir'],
-                                     ref_ps=sample_annotation['fasta_ps'], gff_ps=sample_annotation['gff_ps'])
         sample_names_rep.append(sample_name_rep)
-        process_pip.seq_data_align()
-        thread_exit.append(False)
-        thread.start_new_thread(stat_thread, (process_pip, thread_init))
-        thread_init += 1
-        while sum([True if sg is False else False for sg in thread_exit]) >= threading_max:
-            sleep(10)
+        # process_pip = RNASeqAnalyzer(sample_name_rep, seq_ps1=fa1, seq_ps2=fa2, bowtie_pars={"-p": 32}, output_dir=sampleinfo['save_dir'],
+        #                              ref_ps=sample_annotation['fasta_ps'], gff_ps=sample_annotation['gff_ps'])
+        
+        # process_pip.seq_data_align()
+        # thread_exit.append(False)
+        # thread.start_new_thread(stat_thread, (process_pip, thread_init))
+        # thread_init += 1
+        # while sum([True if sg is False else False for sg in thread_exit]) >= threading_max:
+        #     sleep(10)
 
 
 while False in thread_exit:
@@ -107,6 +107,8 @@ for sample_rep in sample_names_rep:
     # sample_name = sample['name']
     # sample_replicates = list(set([fafile.strip(fafile.split('_')[-1])[0:-1] for fafile in sample_fa]))
     # for rep_index, replicate in enumerate(sample_replicates):
-    cmd_cp = f"cp {os.path.join(save_dir, sample_rep + '_output', sample_rep + '.expression_statistic.csv')} " + \
-             f"{os.path.join(save_dir, sample_rep + '.expression_statistic.csv')}"
+    cmd_cp = f"cp {os.path.join(sampleinfo['save_dir'], sample_rep + '_output', sample_rep + '.expression_statistic.csv')} " + \
+             f"{os.path.join(sampleinfo['save_dir'], sample_rep + '.expression_statistic.csv')}"
     sbps.run(cmd_cp, shell=True)
+
+# %%
